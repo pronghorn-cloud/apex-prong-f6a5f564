3fbba1a0a983193f8d12bb20c657b5e683e4a166
developmentation <139758531+developmentation@users.noreply.github.com>
2026-02-03 09:55:40 -0700

    Frontend changed to react

---
+++ frontend/vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true,
+    port: 3000 // Ensure this matches the port you want to expose
+  }
+})
